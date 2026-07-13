537c9456fab84aa082442bf956e420aa67dfb64d
yaassenahmed2021-del <yaassenahmed2021@gmail.com>
2026-07-13 17:50:58 +0300

    Update valid key in verify.js

--- api/verify.js
+++ api/verify.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         const { key } = req.body;
 
         // قائمة المفاتيح المسموح بها فقط
-        const validKeys = ["manager-20954003481-09"];
+        const validKeys = ["HRRH-73828749-%$@72"];
 
         if (validKeys.includes(key)) {
             // إذا وجد المفتاح
